b74abaa9b8162d488d0bc81ad273539b27c442f8
dapaavol <dpaavola@hotmail.com>
2026-07-07 17:39:06 -0700

    4.2_MeteorFlowingLight complete

---
+++ 4.2_MeteorFlowingLight/src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+
+//initializations for led pins, pwm channels, pwm duties, flow speed. Declare ledCounts
+const byte ledPins[] = {21, 47, 38, 39, 40, 41, 42, 2};
+const byte chns[] = {0, 1, 2, 3, 4, 5, 6, 7};
+const int dutys[] = {0, 0, 0, 0, 0, 0, 0, 0,
+                      1023, 512, 256, 128, 64, 32, 16, 8,
+                      0, 0, 0, 0, 0, 0, 0, 0
+                    };
+int ledCounts;
+int delayTimes = 50;
+void setup() {
+  // get ledCounts size, uses that to iterate through channel and pin arrays to attach with a for loop
+  ledCounts = sizeof(ledPins);
+  for (int i = 0; i < ledCounts; i++) {
+    ledcSetup(chns[i], 1000, 10);
+    ledcAttachPin(ledPins[i], chns[i]);
+  }
+}
+
+void loop() {
+  // 
+  for (int i = 0; i < 16; i++) {
+    for (int j = 0; j < ledCounts; j++) {
+      ledcWrite(chns[j], dutys[i + j]);
+    }
+    delay(delayTimes);
+  }
+  for (int i = 0; i < 16; i++) {
+    for (int j = ledCounts - 1; j > -1; j--) {
+    ledcWrite(chns[j], dutys[i + (ledCounts - 1 -j)]);
+  }
+  delay(delayTimes);
+  }
+}
