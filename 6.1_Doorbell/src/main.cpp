58f8695f28b9f59d4d434cc0303a781bf6a1ec1b
dapaavol <dpaavola@hotmail.com>
2026-07-19 11:51:59 -0700

    6.1_Doorbell complete

---
+++ 6.1_Doorbell/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+
+#define PIN_BUZZER 14
+#define PIN_BUTTON 21
+
+void setup() {
+pinMode(PIN_BUZZER, OUTPUT);
+pinMode(PIN_BUTTON, INPUT);   
+}
+
+void loop() {
+if (digitalRead(PIN_BUTTON) == LOW) {
+  digitalWrite(PIN_BUZZER,HIGH);
+}else{
+  digitalWrite(PIN_BUZZER,LOW);
+}
+}
